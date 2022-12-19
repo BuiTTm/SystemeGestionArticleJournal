fb736b5f3416775101f8a4ed528255acb6fdda75
Felix DB <the2deux@gmail.com>
2022-12-19 14:24:14 -0500

    chore: cleanup users

--- utilisateurs/urls.py
+++ utilisateurs/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path
-from utilisateurs import views
 from .views import SignUpView
 
 urlpatterns = [
-    #Auth
     path("signup/", SignUpView.as_view(), name="signupuser"),
-    #path('signup/', views.signupuser, name = 'signupuser'),
-    #path('logout/', views.logoutuser, name = 'logoutuser'),
-    #path('login/', views.loginuser, name = 'loginuser'),
-
-    #User
-    #path('', views.home, name='home'),
 ]
